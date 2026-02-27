9d8cb69e9c2c8dec24aadf641566f8aa4be76fcc
challavimalanand <challavimalanand@gmail.com>
2026-02-27 12:37:18 +0530

    divorce mutual format added

---
+++ modules/telangana/trial_court/Divorce_mutual.py
@@ -0,0 +1,18 @@
+MODULE_NAME = "Divorce Mutual"
+TEMPLATE = "divorce_mutual.docx"
+
+FIELDS = [
+    ("court_name", "Court Name"),
+    ("year", "Year"),
+    ("suit_for", "Suit for"),
+    ("pet_name", "Petitioner No.1 Name"),
+    ("petitioner", "Petitioner No.1 Full Details"),
+    ("res_name", "Petitioner No.2 Name"),
+    ("respondent", "Petitioner No.2 Full Details"),
+    ("adv_name", "Advocate Name"),
+    ("counsel_for", "Counsel For"),
+    ("address_for_service", "Address for Service"),
+    ("summons_address", "Summons Address"),
+    ("place", "Place"),
+    ("aff_oath", "Name of Deponent"),
+]

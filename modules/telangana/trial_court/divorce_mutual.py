3a0dd22c2dda65f267ab056f40d85006867c25b8
challavimalanand <challavimalanand@gmail.com>
2026-02-27 14:21:26 +0530

    test

---
+++ modules/telangana/trial_court/divorce_mutual.py
@@ -0,0 +1,18 @@
+MODULE_NAME = "Legal Heir Suit"
+TEMPLATE = "legal_heir_case.docx"
+
+FIELDS = [
+    ("court_name", "Court Name"),
+    ("year", "Year"),
+    ("suit_for", "Suit for"),
+    ("pet_name", "Plaintiff Name"),
+    ("petitioner", "Plaintiff Full Details"),
+    ("res_name", "Defendant Name"),
+    ("respondent", "Defendant Full Details"),
+    ("adv_name", "Advocate Name"),
+    ("counsel_for", "Counsel For"),
+    ("address_for_service", "Address for Service"),
+    ("summons_address", "Summons Address"),
+    ("place", "Place"),
+    ("aff_oath", "Name of Deponent"),
+]

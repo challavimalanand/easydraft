f0c201624f4cc0a21ccfed33cd97a38cfb53a236
challavimalanand <challavimalanand@gmail.com>
2026-02-27 14:52:54 +0530

    Test

--- modules/telangana/trial_court/divorce_mutual.py
+++ modules/telangana/trial_court/divorce_mutual.py
@@ -4,6 +4,7 @@ TEMPLATE = "divorce_mutual.docx"
 FIELDS = [
     ("court_name", "Court Name"),
     ("year", "Year"),
+    ("dof", "Date of filing"),
     ("pet_name", "Petitioner No.1 Name"),
     ("petitioner", "Petitioner No.1 Full Details"),
     ("res_name", "Petitioner No.2 Name"),

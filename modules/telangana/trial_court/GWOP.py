4503731c25422a03788f25172c5b96bed94787da
challavimalanand <challavimalanand@gmail.com>
2026-03-11 22:24:01 +0530

    GWOP

---
+++ modules/telangana/trial_court/GWOP.py
@@ -0,0 +1,17 @@
+MODULE_NAME = "GWOP"
+TEMPLATE = "GWOP.docx"
+
+FIELDS = [
+    ("court_name", "Court Name"),
+    ("year", "Year"),
+    ("dof", "Date of Filing"),
+    ("pet_name", "Plaintiff Name"),
+    ("petitioner", "Plaintiff Full Details"),
+    ("res_name", "Defendant Name"),
+    ("respondent", "Defendant Full Details"),
+    ("adv_name", "Advocate Name"),
+    ("address_for_service", "Address for Service"),
+    ("summons_address", "Summons Address"),
+    ("place", "Place"),
+    ("aff_oath", "Name of Deponent"),
+]
